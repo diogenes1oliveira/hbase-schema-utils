cc4228d43e12a9a7e15d68e8a5328367dc48f062
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-02-18 23:13:00 -0300

    feat: first connection try

---
+++ src/main/java/com/github/diogenes1oliveira/hbase/connection/HBaseManualKerberosConnection.java
@@ -0,0 +1,21 @@
+package com.github.diogenes1oliveira.hbase.connection;
+
+import com.github.diogenes1oliveira.hbase.connection.interfaces.HBaseConnectionProxy;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.Connection;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class HBaseManualKerberosConnection extends HBaseConnectionProxy {
+    private final AtomicReference<Connection> connectionRef = new AtomicReference<>();
+
+    public HBaseManualKerberosConnection(Configuration conf, String principal, String keytab) {
+
+    }
+
+    @Override
+    protected Connection getConnection() {
+        return connectionRef.get();
+    }
+
+}
--- src/main/java/com/github/diogenes1oliveira/hbase/schema/utils/HBaseUtils.java
+++ src/main/java/com/github/diogenes1oliveira/hbase/schema/utils/HBaseUtils.java
@@ -1,7 +1,11 @@
 package com.github.diogenes1oliveira.hbase.schema.utils;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import java.util.Map;
+import java.util.Properties;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -37,4 +41,31 @@ public final class HBaseUtils {
         return new TreeMap<>(Bytes.BYTES_COMPARATOR);
     }
 
+    /**
+     * Converts the properties into a HBase configuration
+     *
+     * @param props properties to convert
+     * @return Hadoop configuration object
+     */
+    public static Configuration toHBaseConf(Map<String, String> props) {
+        Configuration configuration = HBaseConfiguration.create();
+
+        for (Map.Entry<String, String> entry : props.entrySet()) {
+            configuration.set(entry.getKey(), entry.getValue());
+        }
+
+        return configuration;
+    }
+
+    /**
+     * Converts the properties into a HBase configuration
+     *
+     * @param props Java properties to convert
+     * @return Hadoop configuration object
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static Configuration toHBaseConf(Properties props) {
+        Map<String, String> propsMap = (Map) props;
+        return toHBaseConf(propsMap);
+    }
 }
