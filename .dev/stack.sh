317ffa4e92ee5dd8a534b3d1dfe4e468db02baa7
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-02-20 18:55:08 -0300

    feat: adding a local test stack

--- .dev/stack.sh
+++ .dev/stack.sh
@@ -6,26 +6,79 @@ usage() {
 Interacts with the development stack
 
 Usage:
-  $SCRIPT [ start | stop ]
+  $SCRIPT [ up | rm | shell ]
 eof
 }
 
-COMMAND=""
+SCRIPT="$0"
 
 main() {
+  function_name="command_${1/-/_}"
+  shift
+
   up_to_root
-  "${COMMAND}"
+  setup_env
+  "${function_name}" "$@"
 }
 
-stack_start() {
+setup_env() {
+  touch .env
+  if ! grep -q HBASE_TEST_INSTANCE_TYPE .env; then
+    {
+      echo
+      echo "# set by .dev/stack.sh on $(date -u --iso=seconds)"
+      echo HBASE_TEST_INSTANCE_TYPE=local
+    } >> .env
+  fi
+}
+
+command_up() {
   cd .dev
+  docker-compose up --remove-orphans --renew-anon-volumes --detach
+  hbase_wait_for_shell
 }
 
-stack_stop() {
+command_rm() {
   cd .dev
+  docker-compose kill -s 9
+  docker-compose rm -fsv
+  docker volume prune -f
+  docker network prune -f
 }
 
-SCRIPT="$0"
+command_shell() {
+  hbase_exec hbase shell
+}
+
+hbase_exec() {
+  flags=-i
+  if [ -t 0 ]; then
+    flags=-it
+  fi
+
+  docker exec "${flags}" hbase "$@"
+}
+
+hbase_wait_for_shell() {
+  i=0
+
+  while true; do
+    i="$((i+1))"
+    if [ "$i" -gt 5 ]; then
+      echo >&2 "ERROR: hbase is still not up after 5 retries, giving up"
+      return 1
+    fi
+    echo >&2 "INFO: checking if hbase is up ($i/5)..."
+    if hbase_exec hbase-shell-run.sh 'list' > /dev/null; then
+      echo >&2 "INFO: hbase is up"
+      break
+    elif ! [ "$i" -eq 5 ]; then
+      echo >&2 "INFO: hbase is still not up, will retry in 1 second..."
+      sleep 1
+    fi
+  done
+}
+
 
 up_to_root() {
   # Directory containing this script
@@ -45,8 +98,8 @@ case "${1:-}" in
   usage
   exit 0
   ;;
-start | stop )
-  COMMAND="stack_$1"
+up | rm | shell)
+  # just to check
   ;;
 *)
   usage >&2
