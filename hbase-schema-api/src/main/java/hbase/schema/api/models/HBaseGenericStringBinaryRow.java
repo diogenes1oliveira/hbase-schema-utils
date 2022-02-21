05c1c1a738add497e530e49e1adc58ab09b40fd2
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-02-21 14:55:26 -0300

    feat: generic models for base64 and string binary

---
+++ hbase-schema-api/src/main/java/hbase/schema/api/models/HBaseGenericStringBinaryRow.java
@@ -0,0 +1,89 @@
+package hbase.schema.api.models;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+import org.apache.hadoop.hbase.shaded.org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.hadoop.hbase.shaded.org.apache.commons.lang.builder.ToStringStyle;
+
+import java.time.Instant;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+
+public class HBaseGenericStringBinaryRow {
+    private String rowKey;
+    private Long timestampMs;
+    private Map<String, String> bytesCells;
+    private Map<String, Long> longCells;
+
+    public HBaseGenericStringBinaryRow(String rowKey,
+                                       @Nullable Long timestampMs,
+                                       Map<String, String> bytesCells,
+                                       @Nullable Map<String, Long> longCells) {
+        this.rowKey = rowKey;
+        this.timestampMs = timestampMs;
+        this.bytesCells = bytesCells;
+        this.longCells = longCells;
+    }
+
+    public HBaseGenericStringBinaryRow(String rowKey,
+                                       @Nullable Long timestampMs,
+                                       Map<String, String> bytesCells) {
+        this(rowKey, timestampMs, bytesCells, emptyMap());
+    }
+
+    public HBaseGenericStringBinaryRow(String rowKey,
+                                       Map<String, String> bytesCells) {
+        this(rowKey, null, bytesCells);
+    }
+
+    public HBaseGenericStringBinaryRow(String rowKey) {
+        this(rowKey, null, emptyMap());
+    }
+
+    public String getRowKey() {
+        return rowKey;
+    }
+
+    public void setRowKey(String rowKey) {
+        this.rowKey = rowKey;
+    }
+
+    @Nullable
+    public Long getTimestampMs() {
+        return timestampMs;
+    }
+
+    public void setTimestampMs(@Nullable Long timestampMs) {
+        this.timestampMs = timestampMs;
+    }
+
+    public Map<String, String> getBytesCells() {
+        return bytesCells;
+    }
+
+    public void setBytesCells(Map<String, String> bytesCells) {
+        this.bytesCells = bytesCells;
+    }
+
+    @Nullable
+    public Map<String, Long> getLongCells() {
+        return longCells;
+    }
+
+    public void setLongCells(@Nullable Map<String, Long> longCells) {
+        this.longCells = longCells;
+    }
+
+    @Override
+    public String toString() {
+        Instant timestamp = timestampMs != null ? Instant.ofEpochMilli(timestampMs) : null;
+
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("row_key", rowKey)
+                .append("timestamp", timestamp)
+                .append("bytes", bytesCells)
+                .append("longs", longCells)
+                .toString();
+    }
+
+}
