e0d10b44e9445cad9c7e3e07712e78d8c417badb
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-03-03 00:36:47 -0300

    feat: javadoc for HBaseSchemaUtils

--- hbase-schema-api/src/main/java/hbase/schema/api/utils/HBaseSchemaUtils.java
+++ hbase-schema-api/src/main/java/hbase/schema/api/utils/HBaseSchemaUtils.java
@@ -9,6 +9,9 @@ import java.util.TreeSet;
 
 import static java.util.Arrays.asList;
 
+/**
+ * Miscellaneous utilities for schemas
+ */
 public final class HBaseSchemaUtils {
     private HBaseSchemaUtils() {
         // utility class
@@ -36,31 +39,49 @@ public final class HBaseSchemaUtils {
         return set;
     }
 
+    /**
+     * Builds a new map keyed by binary bytes
+     *
+     * @return map sorted by bytes values using {@link Bytes#BYTES_COMPARATOR}
+     */
     public static <T> TreeMap<byte[], T> asBytesTreeMap() {
         return new TreeMap<>(Bytes.BYTES_COMPARATOR);
     }
 
+    /**
+     * Builds a new map keyed by binary bytes
+     *
+     * @param key   initial key
+     * @param value initial value
+     * @param <T>   value type
+     * @return map sorted by bytes values using {@link Bytes#BYTES_COMPARATOR}
+     */
     public static <T> TreeMap<byte[], T> asBytesTreeMap(byte[] key, T value) {
         TreeMap<byte[], T> map = new TreeMap<>(Bytes.BYTES_COMPARATOR);
         map.put(key, value);
         return map;
     }
 
+    /**
+     * Builds a new map keyed by binary bytes
+     *
+     * @param firstKey           initial key
+     * @param firstValue         initial value
+     * @param otherKeysAndValues array [key1, value1, key2, value2, ...]
+     * @param <T>                value type
+     * @return map sorted by bytes values using {@link Bytes#BYTES_COMPARATOR}
+     * @throws IllegalArgumentException key without corresponding value in {@code otherKeysAndValues}
+     * @throws IllegalArgumentException value with type not compatible with {@code firstValue}
+     */
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public static <T> TreeMap<byte[], T> asBytesTreeMap(byte[] firstKey, @NotNull T firstValue, Object... otherKeysAndValues) {
-        Class<T> valueClass = (Class) firstValue.getClass();
-        return asBytesTreeMap(valueClass, firstKey, firstValue, otherKeysAndValues);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> TreeMap<byte[], T> asBytesTreeMap(Class<T> valueClass,
-                                                        byte[] firstKey,
-                                                        T firstValue,
+    public static <T> TreeMap<byte[], T> asBytesTreeMap(byte[] firstKey,
+                                                        @NotNull T firstValue,
                                                         Object... otherKeysAndValues) {
         if (otherKeysAndValues.length % 2 != 0) {
             int lastKeyIndex = (otherKeysAndValues.length - 1) / 2;
             throw new IllegalArgumentException("Key #" + lastKeyIndex + " doesn't have a value");
         }
+        Class<T> valueClass = (Class) firstValue.getClass();
         TreeMap<byte[], T> map = new TreeMap<>(Bytes.BYTES_COMPARATOR);
         map.put(firstKey, firstValue);
 
@@ -76,25 +97,35 @@ public final class HBaseSchemaUtils {
         return map;
     }
 
+    /**
+     * Checks if at least one object is non-null
+     *
+     * @param message message for the exception
+     * @param objs    objects to be verified
+     * @throws IllegalStateException couldn't find a non-null object
+     */
     public static void verifyNonNull(String message, Object... objs) {
         for (Object obj : objs) {
             if (obj != null) {
                 return;
             }
         }
-        if (objs.length > 0) {
-            throw new IllegalStateException(message);
-        }
+        throw new IllegalStateException(message);
     }
 
+    /**
+     * Checks if at least one collection is non-empty
+     *
+     * @param message     message for the exception
+     * @param collections collections to be verified
+     * @throws IllegalStateException couldn't find a non-empty collection
+     */
     public static void verifyNonEmpty(String message, Collection<?>... collections) {
         for (Collection<?> collection : collections) {
             if (collection != null && !collection.isEmpty()) {
                 return;
             }
         }
-        if (collections.length > 0) {
-            throw new IllegalStateException(message);
-        }
+        throw new IllegalStateException(message);
     }
 }
