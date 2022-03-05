459672fc3202f8b967987310ff779cf2c19336fa
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-03-05 20:06:01 -0300

    feat: adding Q generic type for filter and queries in HBaseSchema

--- hbase-schema-api/src/main/java/hbase/schema/api/interfaces/HBaseSchema.java
+++ hbase-schema-api/src/main/java/hbase/schema/api/interfaces/HBaseSchema.java
@@ -3,19 +3,25 @@ package hbase.schema.api.interfaces;
 /**
  * Interface to map Java objects to and from HBase
  *
- * @param <T> query and mutation types
- * @param <R> output result type
+ * @param <T> mutation type
+ * @param <Q> query and filter type
+ * @param <R> result type
  */
-public interface HBaseSchema<T, R> {
-    /**
-     * Schema to generate the Gets and Scans queries
-     */
-    HBaseQuerySchema<T> querySchema();
-
+public interface HBaseSchema<T, Q, R> {
     /**
      * Schema to generate the Mutations
      */
     HBaseMutationSchema<T> mutationSchema();
+
+    /**
+     * Schema to generate the Gets and Scans queries
+     */
+    HBaseQuerySchema<Q> querySchema();
+
+    /**
+     * Schema to generate the Filters
+     */
+    HBaseFilterSchema<Q> filterSchema();
 
     /**
      * Schema to parse fetched Results
