5586a1952d63c7da5494e5987be8c2f7a622a177
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-04-30 10:23:19 -0300

    feat: adding HBaseBytesMapper#andThen to post-process the list of byte buffers

--- hbase-schema-api/src/main/java/hbase/schema/api/interfaces/HBaseBytesMapper.java
+++ hbase-schema-api/src/main/java/hbase/schema/api/interfaces/HBaseBytesMapper.java
@@ -2,15 +2,21 @@ package hbase.schema.api.interfaces;
 
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.function.Function;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
 
 @FunctionalInterface
 public interface HBaseBytesMapper<T> {
     HBaseBytesMapper<Object> EMPTY = v -> emptyList();
 
     List<ByteBuffer> toBuffers(T value);
+
+    default HBaseBytesMapper<T> andThen(Function<ByteBuffer, ByteBuffer> mapper) {
+        return value -> toBuffers(value).stream().map(mapper).collect(toList());
+    }
 
     static <T> HBaseBytesMapper<T> singleton(ByteBuffer buffer) {
         List<ByteBuffer> result = singletonList(buffer);
