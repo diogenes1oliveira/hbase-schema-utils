e10c5b5a621cc18ea1534f2399872c5f8022e152
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2022-05-02 08:10:24 -0300

    chore: cleaning up BytesNullableComparator

--- hbase-schema-api/src/main/java/hbase/schema/api/utils/BytesNullableComparator.java
+++ hbase-schema-api/src/main/java/hbase/schema/api/utils/BytesNullableComparator.java
@@ -13,13 +13,16 @@ public class BytesNullableComparator implements Comparator<byte[]> {
 
     @Override
     public int compare(byte[] b1, byte[] b2) {
-        if (b1 == null) {
-            return (b2 == null || b2.length == 0) ? 0 : -1;
-        } else if (b2 == null) {
+        if (isEmpty(b1)) {
+            return isEmpty(b2) ? 0 : -1;
+        } else if (isEmpty(b2)) {
             return 1;
         } else {
             return Bytes.BYTES_COMPARATOR.compare(b1, b2);
         }
     }
 
+    public static boolean isEmpty(byte[] bytes) {
+        return bytes == null || bytes.length == 0;
+    }
 }
